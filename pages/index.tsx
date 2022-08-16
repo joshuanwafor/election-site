67458f8d2683ccceb1f6c782f7fac60e797c6a81
joshuanwafor <joshuanwafor01@gmail.com>
2022-08-16 21:48:31 +0000

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,11 @@ import SiteHeader from '../ui/header/Header'
 const Home: NextPage = () => {
   return (
     <div>
-
       <div className="container col-xxl-8 px-4 py-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5 justify-content-center text-center">
           <div className="col-md-8">
             <h1 className="display-5 fw-bold lh-1 mb-3">Support the on going movement</h1>
-            <p className="lead">This movement is bigger than all of us. Support the on going impact we have around the world and let's continue to build a more sustainable future for ourselves and our kids.</p>
+            <p className="lead">This movement is bigger than all of us. Support the on going impact we have around the world and lets continue to build a more sustainable future for ourselves and our kids.</p>
             <div className="">
               <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Donate</button>
               <button type="button" className="btn btn-outline-secondary btn-lg px-4">Stats</button>
